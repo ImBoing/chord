63e37233e6fa8b56b9bfc9de4e516e7777cedc92
boingfromdiscord <62665535+boingfromdiscord@users.noreply.github.com>
2020-05-22 18:35:47 -0400

    working on modmail

--- src/server.js
+++ src/server.js
@@ -58,18 +58,21 @@ client.on("message", async message => {
   // else {
   const prefix = "la!";
   if (message.author.bot) return;
-  if (!message.guild) return;
-  if (!message.content.startsWith(prefix)) return;
-  const args = message.content
-    .slice(prefix.length)
-    .trim()
-    .split(/ +/g);
-  const cmd = args.shift().toLowerCase();
-  if (cmd.length === 0) return;
-  let command = client.commands.get(cmd);
-  if (!command) command = client.commands.get(client.aliases.get(cmd));
-  if (command) command.run(client, message, args);
-  // }
+  if (message.content.startsWith(prefix)) {
+    const args = message.content
+      .slice(prefix.length)
+      .trim()
+      .split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if (cmd.length === 0) return;
+    let command = client.commands.get(cmd);
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
+    if (command) command.run(client, message, args);
+  }
+
+  const guild = bot.guilds.cache.get("690991118518845464"); // Guild id
+  
+  // }}
 });
 
 client.on("guildMemberAdd", async member => {
