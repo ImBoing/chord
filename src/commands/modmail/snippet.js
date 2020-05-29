0beda4a5956376f43210739e1f05ed289c8e2f75
boingfromdiscord <62665535+boingfromdiscord@users.noreply.github.com>
2020-05-29 14:15:26 -0400

    stage 3 of snippets complete

--- src/commands/modmail/snippet.js
+++ src/commands/modmail/snippet.js
@@ -1,6 +1,9 @@
+/* eslint-disable func-names */
+/* eslint-disable no-useless-concat */
 /* eslint-disable new-cap */
 /* eslint-disable no-console */
 /* eslint-disable prefer-destructuring */
+const { MessageEmbed } = require("discord.js");
 const data = require("../../database/models/snippet.js");
 
 module.exports = {
@@ -38,6 +41,7 @@ module.exports = {
           snip = new data({
             snippetName: name,
             snippetContent: snippet,
+            guild: message.guild.id,
           });
           // Save the snippet
           await snip.save().catch((err) => console.log(err));
@@ -62,6 +66,7 @@ module.exports = {
           single = new data({
             snippetName: name,
             snippetContent: snippet,
+            guild: message.guild.id,
           });
           // Save the snippet
           await single.save().catch((err) => console.log(err));
@@ -77,6 +82,16 @@ module.exports = {
     } else if (condition === "remove") {
       // Used to remove a snippet
       console.log("remove a snippet");
+      const rem = await data.findOne({
+        guild: message.guild.id,
+        snippetName: args.slice(1).join(" "),
+      });
+      if (!rem) {
+        message.channel.send("There seems to be no snippet with that name");
+      } else {
+        rem.deleteOne().catch((err) => console.log(err));
+        message.channel.send("The snippet has succesfully been removed");
+      }
     } else if (condition === "test") {
       // Used to see what a snippet looks like
       console.log("test a snippet");
@@ -87,21 +102,72 @@ module.exports = {
         .then((res) => {
           message.channel.send(res.snippetContent);
         });
-    } else {
-      await data
-        .findOne({
-          snippetName: args.slice(0).join(" "),
+    } else if (condition === "view") {
+      console.log("view all");
+      let sOut;
+      data
+        .find({
+          guild: message.guild.id,
         })
         .then((res) => {
-          console.log(res.snippetContent);
-        });
-    }
+          if (res.length === 0) {
+            sOut = "No snippets have been setup yet";
+          } else {
+            const sName = res.map((z) => z.snippetName);
+            const sContent = res.map((x) => x.snippetContent);
+            const sOutp = sContent.map(function (a, b) {
+              // eslint-disable-next-line prettier/prettier
+              return ["Name:" + ` **${sName[b]}**` + "\n" + "Snippet:" + ` ${a}`];
+            });
+            sOut = sOutp.join("\n\n");
+          }
+          const view = new MessageEmbed()
+            .setColor("RANDOM")
+            .setTitle("ModMail Snippets")
+            .setDescription(sOut);
 
-    // const uno = args.join(" ");
-    // const dos = uno.match(/".*"/g).join(" ");
-    // const tres = dos.replace('"', "");
-    // const quatro = tres.replace('"', "");
-    // const cinco = uno.slice(quatro.length + 3);
-    // console.log(cinco);
+          message.channel.send(view);
+        });
+    } else {
+      const modCat = [
+        "714882756131160074",
+        "714883352510857357",
+        "714882834103533599",
+        "714882905645514782",
+      ];
+      if (modCat.includes(message.channel.parentID)) {
+        try {
+          await data
+            .findOne({
+              snippetName: args.slice(0).join(" "),
+            })
+            .then(async (res) => {
+              const array = message.channel.topic;
+              const id = array.split(" ")[2];
+              const user = client.users.cache.get(id);
+
+              const embed = new MessageEmbed()
+                .setColor("GREEN")
+                .setAuthor(
+                  message.author.tag,
+                  message.author.displayAvatarURL(),
+                  `https://discordapp.com/users/${message.author.id}`
+                )
+                .setDescription(res.snippetContent)
+                .setFooter("Message recieved")
+                .setTimestamp();
+
+              user.send(embed);
+              message.channel
+                .send("I have succesfully sent the message to the user")
+                .then((msg) => {
+                  msg.delete({ timeout: 5000 });
+                });
+            });
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }
   },
 };
--- src/database/models/snippet.js
+++ src/database/models/snippet.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cpSchema = mongoose.Schema({
   snippetName: String,
   snippetContent: String,
+  guild: String,
 });
 
 module.exports = mongoose.model("snippets", cpSchema);
