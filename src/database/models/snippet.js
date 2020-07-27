e6766b2dccac84e2c75bf14fb13d8d35a32f9be8
Tyler Lo <tylerlo@Tylers-MacBook-Pro.local>
2020-07-27 16:07:59 -0400

    Big updates

--- src/database/models/snippet.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const cpSchema = mongoose.Schema({
-  snippetName: String,
-  snippetContent: String,
-  guild: String,
-});
-
-module.exports = mongoose.model('snippets', cpSchema);
