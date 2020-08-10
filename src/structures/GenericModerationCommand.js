0c5e434d81d555f6a32de733bde82ce707fc9281
Boingfromdiscord <mr.manguydudee@gmail.com>
2020-08-10 10:52:16 -0400

    hello

--- src/structures/GenericModerationCommand.js
+++
@@ -1,8 +0,0 @@
-const GenericCommand = require('./GenericCommand.js');
-
-module.exports = class GenericModerationCommand extends GenericCommand {
-  // eslint-disable-next-line no-unused-vars
-  async run(message, args) {
-    throw new Error(`Command ${this.name} does not have a run method`);
-  }
-};
--- start.sh
+++
@@ -1,1 +0,0 @@
-Testing stuff ya know
